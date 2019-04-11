ed625857df7ef2c61ead449847687e67643b7b08
Anthony <anthonymtamasi@gmail.com>
2019-04-10 20:28:46 -0400

    Starting with simple text based UI. May use PyQt to implement a GUI later

---
+++ ui.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 10 17:04:56 2019
+
+@author: amtam
+"""
+
+def main():
+    #year = input("Year?: ")
+    #week = input("Week?: ")
+    print()
+    
+    #Get the relevant data from our data storage
+    #Will be based on architecture of dataframe
+        
+    
+    #Calculate lineup with the most points possible
+    #Get top players for each position for the year, week combination
+    
+    
+    #Calculate "best" lineup for FanDuel possible
+    #Knapsack problem approach
+    
+    
+    #Print out our optimal lineups
+    
+    print("The best possible lineup:")
+    print("Position  Name \t\t Points")
+    print()
+    print(3 * "\n")
+    #Print out total points scored
+    
+    
+    
+    print("The best 'bang for your buck' lineup:")
+    print("Position  Name \t\t Salary \t Points")
+    
+    
+    print(3 * "\n")
+    #Print out total points scored and total salary
+    
+    
+    
+main()
