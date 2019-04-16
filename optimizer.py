2e85a1a609235eae7f2ffe4c529b5ff0726b0669
Anthony <anthonymtamasi@gmail.com>
2019-04-16 15:03:01 -0400

    Began implementation of Optimizer class. Barebones of the init, knapsack, and maxLineup functions

---
+++ optimizer.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+class Optimizer():
+    def __init__(self):
+
+        self.data = self.readData()
+
+        
+
+    def knapsack():
+        #Return the optimal lineup when we consider player salaries
+
+    def max_lineup():
+        #Return maximum possible lineup 
